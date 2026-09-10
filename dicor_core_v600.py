123cdd9a87662ee1eb2d79ba40e83669f4398d3c
matheusviana6743-cell <matheusviana6743@gmail.com>
2026-09-10 20:45:39 -0300

    chore: remover entrypoint antigo do core V600

--- dicor_core_v600.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Entry point for the single clean DICOR V600 core."""
-from dicor_core_v600_impl import install, generate_doc, Core
-__all__ = ['install', 'generate_doc', 'Core']
